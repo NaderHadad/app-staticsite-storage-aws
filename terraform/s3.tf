d6720bbd0602bac9755eb2a5fb075ecf74920f6f
Nader Hadad Souza <naderhadadsouza@gmail.com>
2023-12-06 21:34:00 -0300

    limpa

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -11,6 +11,33 @@ resource "aws_s3_bucket_versioning" "bucket-versioning" {
   }
 }
 
+resource "aws_iam_policy" "s3_acl_policy" {
+  name        = "s3_acl_policy"
+  description = "Policy for modifying S3 ACLs"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "s3:PutBucketAcl",
+          "s3:PutObjectAcl",
+        ],
+        Effect   = "Allow",
+        Resource = [
+          aws_s3_bucket.bucket.arn,
+          "${aws_s3_bucket.bucket.arn}/*",
+        ],
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_acl_attachment" {
+  policy_arn = aws_iam_policy.s3_acl_policy.arn
+  role       = aws_iam_role.terraform_execution_role.name
+}
+
 resource "aws_s3_bucket_acl" "bucket-acl" {
   bucket = aws_s3_bucket.bucket.id
   acl    = "public-read"
